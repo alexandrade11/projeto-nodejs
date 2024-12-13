3c459bef3a720a7d66712d4a593a10129d5da4f0
Guilherme <guilherme.cardoso26876@al.aememmartins.pt>
2024-12-13 15:53:31 +0000

    aa

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/tarefas', (req, res) => {
 app.get('/pagamentos', (req, res) => {
     res.sendStatus (403);
 });
-app.get('/user/:name/natiounality/:country', (req, res) =>{
+app.get('/user/:name/nationality/:country', (req, res) =>{
     const reqn= req.params.name;
     const reqc= req.params.country;
     res.send(`O ${reqn} tem nacionalidade ${reqc}`);
