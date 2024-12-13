fc6385773aa5aecbdbcc8642b07fcae72be0de1b
Guilherme <guilherme.cardoso26876@al.aememmartins.pt>
2024-12-13 15:49:26 +0000

    aula 2

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.get('/tarefas', (req, res) => {
 app.get('/pagamentos', (req, res) => {
     res.sendStatus (403);
 });
-
+app.get('/user/:name/natiounality/:country', (req, res) =>{
+    const reqn= req.params.name;
+    const reqc= req.params.country;
+    res.send(`O ${reqn} tem nacionalidade ${reqc}`);
+}) 
 
 const PORT = 3001;
 app.listen(PORT, () => {
