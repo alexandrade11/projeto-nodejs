b371dbdc9e8a472c08dee8031e9767cfed8d2c2e
Rodrigo <rodrigo.encarnacao26864@al.aememmartins.pt>
2024-12-20 15:36:37 +0000

    aa

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -91,27 +93,46 @@ const users = [
 let currentId = 2;
  
 app.get('/users', (req, res) =>{
-    response.send(users)
+    res.send(users)
   })
  
   app.post('/users', (req, res) =>{
     const nuser = req.body;
+    users.id=currentId;
+    currentId=currentId+1;
     users.push(nuser)
     res.sendStatus(200);
   });
+
  
   app.put('/users', (req, res) =>{
-    
+    for(let i = 0 ; i < users.length; i++){  
+
+        if (users[i].id==req.body.id){
+            if(req.body.first_name != null) {
+                users[i].first_name = req.body.first_name;
+            }
+            if(req.body.last_name != null) {
+          users[i].last_name = req.body.last_name;
+            }
+          if(req.body.email != null) {
+          users[i].email = req.body.email;
+          }
+          res.sendStatus(200);
+        }
+     }
   });
  
   app.delete('/users', (req, res) =>{
-   
-    
-    response.sendStatus(200);
- 
-   
-  }
-  );
+    for(let i = 0 ; i < users.length; i++){  
+
+        if (users[i].id==req.body.id){
+          users.splice(i,1);
+          res.sendStatus(200);
+        }
+     }
+})
+  ;
  
  
 const PORT = 3001;
